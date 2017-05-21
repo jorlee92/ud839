a36c5d3e479699d365a5ddb70c313d8c28dd6628
jorlee92 <jorlee92@gmail.com>
2017-05-20 21:40:05 -0700

    No longer uses arrays to generate the arraylists, translations are now correct, and Images are shown next to the individual words.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,17 +7,27 @@ package com.example.android.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mResourceId;
+
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
-
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+    public int getResourceId() {
+        return mResourceId;
+    }
+
 
     public Word(String mDefaultTranslation, String mMiwokTranslation) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+    }
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int imageID){
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mResourceId = imageID;
     }
 
 
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -49,6 +50,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         miwokText.setText(currentWord.getMiwokTranslation());
         TextView defaultText = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultText.setText(currentWord.getDefaultTranslation());
+        ImageView wordImage = (ImageView) listItemView.findViewById(R.id.imageView);
+        wordImage.setImageResource(currentWord.getResourceId());
         return listItemView;
     }
 }
